c5c2ea0d40a907224242d5404744d0ca486cd750
megha <msamala21@gmail.com>
2021-12-07 08:36:52 -0600

    added index.js in apiroutes

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,15 @@
 const router = require('express').Router()
 const { notes } = require('../../db/db.json')
 const { createNewNote } = require('../../lib/notes');
-const uniqueid = require('uniqueid')
+const { v4: uuidv4 } = require('uuid');
+
 
 router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 router.post('/notes', (req, res) => {
-    req.body.id = uniqueid();
+    req.body.id = uuidv4();
   
     const note = createNewNote(req.body, notes);
     res.json(note);
